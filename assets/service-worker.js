2e987a0f322d15b7204416b7ff37eb6be06e7d8a
Pablo Villar <113723139+pablovdcf@users.noreply.github.com>
2025-11-01 09:21:35 +0100

    Refactor service worker install event listener

--- assets/service-worker.js
+++ assets/service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', (e) => {
     e.waitUntil(caches.open('boda-cache-v1').then(cache => cache.addAll([
-        '/', '/index.html', '/invite.html', '/admin.html',
-        '/assets/api.js', '/assets/app.js', '/assets/admin.js', '/assets/manifest.json'
+        'index.html','invite.html','admin.html',
+        'assets/api.js','assets/app.js','assets/admin.js','assets/manifest.json'
     ])));
 });
 
@@ -10,4 +10,5 @@ self.addEventListener('fetch', (e) => {
     e.respondWith(
         caches.match(e.request).then(resp => resp || fetch(e.request))
     );
+
 });
